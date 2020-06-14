25a3c23383572cef61714a75610e39f9a3bc3c4d
felipemuner <felipe.muner@gmail.com>
2020-06-14 15:14:28 +0700

    nextjs example traversy media

---
+++ components/Prices.js
@@ -0,0 +1,53 @@
+class Prices extends React.Component {
+  state = {
+    currency: "GBP",
+  };
+  render() {
+    let list = "";
+    if (this.state.currency === "USD") {
+      list = (
+        <li className="list-group-item">
+          Bitcoin rate for {this.props.bpi.USD.description} :
+          <span className="badge badge-primary">
+            {this.props.bpi.USD.code} <strong>{this.props.bpi.USD.rate}</strong>
+          </span>
+        </li>
+      );
+    } else if (this.state.currency === "GBP") {
+      list = (
+        <li className="list-group-item">
+          Bitcoin rate for {this.props.bpi.GBP.description} :
+          <span className="badge badge-primary">
+            {this.props.bpi.GBP.code} <strong>{this.props.bpi.GBP.rate}</strong>
+          </span>
+        </li>
+      );
+    } else if (this.state.currency === "EUR") {
+      list = (
+        <li className="list-group-item">
+          Bitcoin rate for {this.props.bpi.EUR.description} :
+          <span className="badge badge-primary">
+            {this.props.bpi.EUR.code} <strong>{this.props.bpi.EUR.rate}</strong>
+          </span>
+        </li>
+      );
+    }
+
+    return (
+      <div>
+        <ul className="list-group">{list}</ul>
+        <br />
+        <select
+          onChange={(e) => this.setState({ currency: e.target.value })}
+          className="form-control"
+        >
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+          <option value="EUR">EUR</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default Prices;
